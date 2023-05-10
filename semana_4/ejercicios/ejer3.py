4aa3a7ce64b45418b50f7bd4cf29f4cad3d173bb
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-05-09 23:59:33 -0300

    semana 4: ejercicio 3

---
+++ semana_4/ejercicios/ejer3.py
@@ -0,0 +1,7 @@
+""" 3-Crea una función llamada invertir_cadena que tome una cadena de texto como
+parámetro y devuelva la cadena invertida. """
+
+def invertir_cadena(texto):
+    return texto[::-1]
+
+print(invertir_cadena("hola mundo"))
