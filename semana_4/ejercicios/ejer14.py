75bc436d2242e7e9373f59100613fc6b7e43f63c
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-05-10 20:13:22 -0300

    semana 4: ejercicio 14

---
+++ semana_4/ejercicios/ejer14.py
@@ -0,0 +1,8 @@
+""" 14-Crea una función llamada encontrar_maximo que tome una lista de números
+como parámetro y devuelva el número máximo de la lista. """
+
+
+def encontrar_maximo(numeros):
+    return max(numeros)
+
+print(encontrar_maximo([10, 100, 5, 55, 2]))
