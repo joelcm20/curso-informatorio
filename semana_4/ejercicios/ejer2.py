2120594a6a0e16c50091adbc029464b7083e30d6
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-05-09 23:56:23 -0300

    semana 4: ejercicio 2

---
+++ semana_4/ejercicios/ejer2.py
@@ -0,0 +1,7 @@
+""" 2-Crea una función llamada saludo que tome el nombre de una persona como
+parámetro e imprima un saludo personalizado. """
+
+def saludo(nombre):
+    print(f"Bienvenido {nombre}")
+
+saludo("messi")
