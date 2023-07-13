c376d0496844bb53b94ac9ac7550eacabf545e8e
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-07-13 00:25:46 -0300

    actualizacion de los modelos de la app news

--- semana_10/ejercicios/django/apps/news/models.py
+++ semana_10/ejercicios/django/apps/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image as Im
 
 # Create your models here.
 
@@ -17,3 +18,18 @@ class News(models.Model):
     publication_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to="news/")
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self):
+        super().save()
+        img = Im.open(self.image.path)
+        # resize it
+        if img.height > 300 or img.width > 300:
+            output_size = (300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
+
+    class Meta:
+        verbose_name_plural = "News"
