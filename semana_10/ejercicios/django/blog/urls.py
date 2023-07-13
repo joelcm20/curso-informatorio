1df09b1dafacb9e21246c395dd46b98a87fbcce6
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-07-13 12:42:39 -0300

    vista home

--- semana_10/ejercicios/django/blog/urls.py
+++ semana_10/ejercicios/django/blog/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls, name="admin"),
-    path("news/", include("apps.news.urls"))
+    path("", views.Home, name="home"),
+    path("news/", include("apps.news.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
