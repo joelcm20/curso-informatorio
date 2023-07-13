5b60f4712edf60ad31824dba84d220bb40f5ebf9
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-07-13 00:25:06 -0300

    actualizacion de las urls de la app news

--- semana_10/ejercicios/django/apps/news/urls.py
+++ semana_10/ejercicios/django/apps/news/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from apps.news import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
-    path("", views.News, name="news"),
-    path("detail/", views.DetailNews, name="detail-news"),
+    path("", views.GetNews, name="news"),
+    path("detail/<int:id>", views.DetailNews, name="detail-news"),
     path("create/", views.CreateNews, name="create-news")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
