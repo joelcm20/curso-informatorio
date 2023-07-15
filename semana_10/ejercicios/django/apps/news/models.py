d23741b6394f336c8b5b60d8df3341c016360879
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-07-14 23:26:15 -0300

    agregar foreignkey al modelo news

--- semana_10/ejercicios/django/apps/news/models.py
+++ semana_10/ejercicios/django/apps/news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from PIL import Image as Im
 from uuid import uuid4
 
@@ -17,6 +18,7 @@ class News(models.Model):
     publication_date = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to="news/")
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
