7db77ee6f2f6e699f641226f40f74091cdece0e3
joelcm20 <132691926+joelcm20@users.noreply.github.com>
2023-07-16 01:01:23 -0300

    funcionalidad eliminar comentarios

--- semana_10/ejercicios/django/apps/comment/urls.py
+++ semana_10/ejercicios/django/apps/comment/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.createComment, name="create-comment")
+    path("create/", views.createComment, name="create-comment"),
+    path("delete/<int:id>", views.deleteComment, name="delete-comment")
 ]
--- semana_10/ejercicios/django/apps/comment/views.py
+++ semana_10/ejercicios/django/apps/comment/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, get_object_or_404
+from django.http import HttpResponse
 from apps.news.models import News
 from .models import Comment
 
@@ -13,3 +14,18 @@ def createComment(request):
         user = request.user
         Comment.objects.create(news=news, user=user, text=comment)
         return redirect("detail-news", id=news_id)
+
+def deleteComment(request, id):
+    if not request.method == "POST":
+        return HttpResponse(status=404)
+    
+    comment = get_object_or_404(Comment, id=id)
+    user = request.user
+    if not comment.user == user:
+        return redirect("detail-news", comment.news.id)
+    
+    Comment.delete(comment)
+    return redirect("detail-news", comment.news.id)
+
+    
+
